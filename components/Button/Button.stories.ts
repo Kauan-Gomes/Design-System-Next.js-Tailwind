79253427d04ad3348eef71b9c44ae8602925696e
Kauan Gomes <Kauang13@gmail.com>
2023-09-06 18:04:48 -0300

    aula 4

--- components/Button/Button.stories.ts
+++ components/Button/Button.stories.ts
@@ -4,7 +4,17 @@ import Button, { ButtonProps } from "./Button"
 const meta: Meta<ButtonProps> = {
     title: 'Moleculas/Button',
     component: Button,
-    argTypes: {}
+    argTypes: {
+        children:{
+            type: "string"
+        },
+        disabled: {
+            type: "boolean",
+        },
+        className: {
+            type: "string"
+        }
+    }
 }
 
 export default meta
@@ -14,10 +24,58 @@ export const Primary: StoryObj<ButtonProps> = {
     children: 'Botão'
     }
 }
+export const Disabled: StoryObj<ButtonProps> = {
+    args: {
+    children: 'Botão', 
+    disabled: true
+    }
+}
+export const Secondary: StoryObj<ButtonProps> = {
+    args: {
+    children: 'Botão',
+    variant: "secondary",
+    }
+}
+export const SecondaryDisabled: StoryObj<ButtonProps> = {
+    args: {
+    children: 'Botão',
+    variant: "secondary",
+    disabled: true,
+    }
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+    args: {
+    children: 'Botão',
+    variant: "tertiary",
+    }
+}
+export const TertiaryDisabled: StoryObj<ButtonProps> = {
+    args: {
+    children: 'Botão',
+    variant: "tertiary",
+    disabled: true,
+    }
+}
 
 export const Violet: StoryObj<ButtonProps>={
     args:{
         children: 'Botão',
-        className: 'theme-violet'
+        className: 'theme-violet',
+    }
+}
+
+export const VioletSecondary: StoryObj<ButtonProps> = {
+    args: {
+    children: 'Botão',
+    variant: "secondary",
+    className: 'theme-violet',
+    }
+}
+export const VioletTertiary: StoryObj<ButtonProps> = {
+    args: {
+    children: 'Botão',
+    className: 'theme-violet',
+    variant: "tertiary",
     }
 }
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,22 +1,37 @@
+import { Primary } from "./Button.stories";
+
 export type ButtonProps = {
     children: React.ReactNode;
+    variant?: 'primary' | 'secondary' | 'tertiary';
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 
-const Button = ({ children, className, disabled, ...rest }: ButtonProps) => {
-    const generalStyle = 'rounded-md px-6 py-2'
-    const Btn = (classes: string) => {
-        return (
+
+
+function getVariant(variant: ButtonProps['variant'], disabled: ButtonProps['disabled']) {
+    switch (variant) {
+        case 'primary':
+            return disabled ? 'bg-disabled text-disabled' : 'bg-primary text-white'
+        case 'secondary':
+            return disabled ? 'bg-disabled text-disabled' : 'bg-quaternary text-primary'
+        case 'tertiary':
+        default:
+            return disabled ? 'bg-disabled text-disabled' : 'text-primary'
+    }
+    
+}
+
+
+const Button = ({ variant = 'primary', children, className, disabled, ...rest }: ButtonProps) => {
+    return (
         <button
-            className={` ${generalStyle} ${classes} ${className}  `}
+            className={` rounded-md px-6 py-2 ${getVariant(variant, disabled)} ${className}  `}
             disabled={disabled}
             {...rest}
         >
             {children}
         </button>)
-    }
 
-    return Btn(disabled ? 'bg-bg-disabled text-text-disabled' : 'bg-primary text-white')
 }
 
 
